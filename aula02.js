80d7e16f8e44d4ae7190b656d86f87e11027ef4d
Oreia seca <kaio.maess@escola.pr.gov.br>
2026-05-12 10:27:50 -0300

    Add files via upload

--- aula02.js
+++ aula02.js
@@ -1,10 +1,28 @@
   //DEFINIÇÃO DE VARIÁVEIS
 
    let pessoa = "Nome da pessoa";
-   let idadedePessoa = 30;
+   let idadePessoa = 30;
    let alturaPessoa = 1.93;
    let statusPessoa = true;
    let salarioPessoa = 1500;
    let salarioBonus = 100;
 
    let salarioTotal = salarioBonus + salarioPessoa;
+
+   console.log( pessoa );
+   console.log( idadePessoa );
+   console.log( alturaPessoa );
+   console.log( statusPessoa );
+   console.log( salarioPessoa );
+   console.log( salarioBonus );
+   console.log( salarioTotal ); 
+
+   //UNINDO TODAS AS INFORMAÇÕES EM UM TEXTO:
+   console.log(
+    'O nome digitado foi: '+pessoa+
+    ' a idade digitada foi: '+idadePessoa+
+    ' a altura digitada foi: '+alturaPessoa+
+    ' o status da pessoa é: '+statusPessoa+
+    ' o salário da pessoa é :' +salarioPessoa+
+    ' e o salario bonus é : '+salarioTotal+ '!!!'
+   )
