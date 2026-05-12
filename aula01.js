cde5759bf4a7a41289a30f08cad68365d62e5506
Oreia seca <kaio.maess@escola.pr.gov.br>
2026-05-12 08:30:07 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,8 @@
+//alert("O carpa capim fica batendo no amiguinho!!!")
+console.log("primeiro código em JS");
+console.log(4 + 5);
+console.log(3 * 4);
+console.log(true);
+console.log(3.89);
+
+console.log("Thiago" + " " + 23 + " " + 1.83 + " " + true);
