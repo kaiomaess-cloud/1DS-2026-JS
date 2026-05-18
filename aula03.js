f7e26364504570ff95336b96433cede72c796ec3
Oreia seca <kaio.maess@escola.pr.gov.br>
2026-05-18 09:21:10 -0300

    Add files via upload

---
+++ aula03.js
@@ -0,0 +1,27 @@
+let nome = "Thiago";
+
+if (nome == "Thiago ") {
+console.log("Voce digitou o nome correto!");
+}else { 
+console.log("Voce digitou outra coisa...");
+}
+
+let idade = 17;
+
+if (idade == 18) { 
+  console.log("Voce é praticamente adulto...");
+   }else if(idade < 18) { 
+      console.log("Voce é ainda é de menor");
+ }else{
+      console.log("Voce é adulto");    
+ }
+
+ //FAÇA UMA VERIFICAÇÃO COM IF E ELSE
+ //DE UMA PESSOA SE ELA PODE OU NÃO VOLTAR
+
+
+ let idade = 16;
+if (idade = 16 )
+console.log("Voce pode votar...")
+if (idade <= 16)      
+console.log("Voce não pode votar...")
